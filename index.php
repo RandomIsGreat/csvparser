2f8feb6bb2052fd865c775a13f7e3cd9b2232b62
RandomIsGreat <ragnar765@yandex.ru>
2021-03-07 14:44:38 +0300

    Add some comments and array_merge to parser

--- index.php
+++ index.php
@@ -2,39 +2,31 @@
 
 if(isset($_FILES['csv'])) {
     $xml = simplexml_load_file("http://localhost/dataBaseConnect.xml");
-    $dsn = "{$xml->dbtype}:dbname={$xml->dbname};host={$xml->host}";
+    $dsn = "{$xml->dbtype}:dbname={$xml->dbname};host={$xml->host}/";
     $username = (string)$xml->username;
-    $db = new PDO($dsn, $username);
-
+    //add $password = (string)$xml->password; optionally
+    $db = new PDO($dsn, $username); //add $password as argument to PDO optionally
     $csv = fopen($_FILES['csv']['tmp_name'], 'r');
     $rowToParse = fgetcsv($csv, '1000', ';');
-
     $tableCreationSql = "CREATE TABLE IF NOT EXISTS {$xml->tablename} ( ";
     $tableCreationData = [];
     $inputString = '(';
-
     foreach ($rowToParse as $el) {
             $inputString .='?, ';
             $tableCreationSql .= " $el varchar(255) NULL,";
             $tableCreationData[] = $el;
     }
-
     $tableCreationSql = mb_substr($tableCreationSql, 0, -1);
     $tableCreationSql .= ')';
     $inputString = mb_substr($inputString, 0, -2);
     $inputString .=')';
-
     $sth = $db->prepare($tableCreationSql);
     $sth->execute([]);
-
     $additionToDataBase = $inputString;
     $tableData = [];
     $isFirst = true;
-
     while ($rowToParse = fgetcsv($csv, '1000', ';')) {
-        foreach ($rowToParse as $el) {
-            $tableData[] = $el;
-        }
+        $tableData = array_merge($tableData, $rowToParse);
         if ($isFirst) {
             $isFirst = false;
         } else {
@@ -42,21 +34,16 @@ if(isset($_FILES['csv'])) {
         }
     }
     fclose($csv);
-    $collumnsForSql = '(';
-
+    $columnsForSql = '(';
     foreach ($tableCreationData as $el) {
-        $collumnsForSql .= "$el, ";
+        $columnsForSql .= "$el, ";
     }
-
-    $collumnsForSql = mb_substr($collumnsForSql, 0, -2);
-    $collumnsForSql .= ')';
-
-    $sqlForAddition = "INSERT INTO `{$xml->tablename}` $collumnsForSql
-                    VALUES $additionToDataBase";
-
+    $columnsForSql = mb_substr($columnsForSql, 0, -2);
+    $columnsForSql .= ')';
+    $sqlForAddition = "INSERT INTO `{$xml->tablename}` $columnsForSql
+                       VALUES $additionToDataBase";
     $sth = $db->prepare($sqlForAddition);
     $sth->execute($tableData);
-
     $sth = $db->prepare("SELECT * FROM `{$xml->tablename}`");
     $sth->execute([]);
     $result = $sth->fetchAll(PDO::FETCH_ASSOC);
